5889327bcf7e704f59fe3561a09f32cd4c3541a9
Can Yıldız <canyildiz@gmail.com>
2019-01-01 17:48:02 +0300

    Iteration 4, UC 5.8 Realized

--- METU.VRS/Controllers/StaffController.cs
+++ METU.VRS/Controllers/StaffController.cs
@@ -27,7 +27,7 @@ namespace METU.VRS.Controllers
         [HttpGet]
         public ActionResult ListVisitors(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            Trace.WriteLine("GET /Staff/Visitors/List");
+            Trace.WriteLine("GET /Staff/ListVisitors");
 
             ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
@@ -38,61 +38,19 @@ namespace METU.VRS.Controllers
 
             ViewBag.CurrentFilter = searchString;
 
-            IQueryable<Visitor> visitors = null;
+            IPagedList<StickerApplication> visitorApplications = ListApplications(sortOrder, currentFilter, searchString, page, StickerApplicationStatus.WaitingForApproval);
 
-            using (DatabaseContext db = GetNewDBContext())
-            {
-                visitors = db.Visitors
-                    .AsNoTracking()
-                    .Include(a => a.Vehicle)
-                    .Include(a => a.User)
-                    .Where(a => a.Status == VisitorStatus.WaitingForArrival);
-
-                if (!string.IsNullOrEmpty(searchString))
-                {
-                    visitors = visitors.Where(a =>
-                    a.Name.Equals(searchString)
-                    || a.Email.Contains(searchString)
-                    || a.Description.Contains(searchString)
-                    || a.Vehicle.PlateNumber.Contains(searchString)
-                    || a.Vehicle.RegistrationNumber.Contains(searchString)
-                    || a.Vehicle.OwnerName.Contains(searchString));
-                }
-
-                switch (sortOrder)
-                {
-                    case "name_desc":
-                        visitors = visitors.OrderByDescending(v => v.Name);
-                        break;
-                    case "Date":
-                        visitors = visitors.OrderBy(v => v.VisitDate);
-                        break;
-                    case "date_desc":
-                        visitors = visitors.OrderByDescending(v => v.VisitDate);
-                        break;
-                    case "Plate":
-                        visitors = visitors.OrderBy(v => v.Vehicle.PlateNumber);
-                        break;
-                    case "plate_desc":
-                        visitors = visitors.OrderByDescending(v => v.Vehicle.PlateNumber);
-                        break;
-                    default:
-                        visitors = visitors.OrderBy(v => v.Status).OrderBy(v => v.LastModified);
-                        break;
-                }
-
-                int pageSize = 10;
-                int pageNumber = (page ?? 1);
-                pageNumber = pageNumber > 0 ? pageNumber : 1;
-                return View(visitors.ToPagedList(pageNumber, pageSize));
-            }
+            return View(visitorApplications);
         }
 
         public ActionResult VisitorArrived(int id)
         {
             using (DatabaseContext db = GetNewDBContext())
             {
-                Visitor visitor = db.Visitors.Include("Vehicle").FirstOrDefault(v => v.ID == id);
+                Visitor visitor = db.Visitors
+                    .Include("Vehicle")
+                    .Include("User")
+                    .FirstOrDefault(v => v.ID == id);
                 if(null != visitor)
                 {
                     visitor.Status = VisitorStatus.Arrived;
@@ -103,7 +61,7 @@ namespace METU.VRS.Controllers
                     }
                     catch (Exception ex)
                     {
-                        return RedirectToAction("ListVisitors", new { err = "Visitor not updated! " + ex.Message });
+                        return RedirectToAction("ListVisitors", new { err = "Visitor not updated: [" + ex.Message + "]"});
                     }
                 }
                 else
